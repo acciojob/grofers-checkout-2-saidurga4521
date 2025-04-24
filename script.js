05d12fbaea5d737dbc71cefbe0ac3a8f9de80510
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-04-24 13:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+
+let priceCells=document.querySelectorAll('[data-ns-test="prices"]');
+let total=0;
+priceCells.forEach((cell)=>{
+	total+=Number(cell.textContent)
+})
+let table=document.getElementById("grocery");
+let row=document.createElement('tr');
+let td=document.createElement('td');
+td.setAttribute("data-ns-test", "grandTotal");
+td.colSpan=2;
+td.textContent = total;
+row.appendChild(td);
+table.appendChild(row);
+
+										
+										 
